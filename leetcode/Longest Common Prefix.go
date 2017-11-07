dadcf437306133d97563cf4327bcf295e1a82eaf
Dapangmao <hchao8@gmail.com>
2017-11-07 14:30:37 -0500

    Create Longest Common Prefix.go

---
+++ leetcode/Longest Common Prefix.go
@@ -0,0 +1,14 @@
+func longestCommonPrefix(strs []string) string {
+    if len(strs) == 0 {
+        return ""
+    }
+    first := strs[0]
+    for i:=0; i<len(first); i++ {
+        for _, str := range strs[1:] {
+            if i >= len(str) || str[i] != first[i] {
+                return first[:i]
+            }
+        }
+    }
+    return first
+}
