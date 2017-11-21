75010751c20755db348ec2a4198425c1c4baccfb
Dapangmao <hchao8@gmail.com>
2017-11-21 10:04:05 -0500

    Update Self Dividing Numbers.go

--- leetcode/Self Dividing Numbers.go
+++ leetcode/Self Dividing Numbers.go
@@ -1,19 +1,16 @@
 func selfDividingNumbers(left int, right int) []int {
     res := []int{}
     for x:=left; x<=right; x++ {
-        is_dividable := true
         y := x
         for y > 0 {
             current := y % 10
             if current == 0 || x % current != 0 {
-                is_dividable = false
-                break 
+                goto end
             }
             y /= 10
         }
-        if is_dividable {
-            res = append(res, x)
-        }
+        res = append(res, x)
+        end:
     }
     return res
 }
