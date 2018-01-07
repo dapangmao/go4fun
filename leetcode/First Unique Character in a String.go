8cd9ae8c5f2d8704b517618d0d9f2ddd2e3cf044
Dapangmao <hchao8@gmail.com>
2018-01-07 15:18:16 -0500

    Update First Unique Character in a String.go

--- leetcode/First Unique Character in a String.go
+++ leetcode/First Unique Character in a String.go
@@ -1,12 +1,6 @@
 func firstUniqChar(s string) int {
     m := make(map[rune]int)
-    for _, x := range s {
-        if val, ok := m[x]; ok {
-            m[x] = val + 1
-        } else {
-            m[x] = 1
-        }
-    }
+    for _, x := range s {m[x]++}
     for i, x := range s {
         if m[x] == 1 {return i}
     }
