0ed7fc0c39890de2d779c91ba3ef556528bbe2bb
Dapangmao <hchao8@gmail.com>
2018-01-07 16:52:07 -0500

    Update Keyboard Row.go

--- leetcode/Keyboard Row.go
+++ leetcode/Keyboard Row.go
@@ -10,7 +10,7 @@ func findWords(words []string) []string {
     }
     var res []string
     for _, w := range words {
-        var current = strings.toLowerCase(w)
+        var current = strings.ToLower(w)
         var first = dict[current[0]]
         for _, c := range current {
             if first != dict[c] {goto end}
