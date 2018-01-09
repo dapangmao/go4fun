a33f9932588a4c389264e0d09cee5966322819ed
Dapangmao <hchao8@gmail.com>
2018-01-09 13:47:24 -0500

    Create Maximum Depth of Binary Tree.go

---
+++ leetcode/Maximum Depth of Binary Tree.go
@@ -0,0 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ 
+func maxDepth(root *TreeNode) int {
+    if root == nil {return 0}
+    l, r := maxDepth(root.Left), maxDepth(root.Right)
+    if l > r {return 1 + l}
+    return 1 + r
+}
