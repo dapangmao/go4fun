064ea13b39bafbda07d44407a169a43a38795c56
Dapangmao <hchao8@gmail.com>
2018-01-23 20:23:59 -0500

    Create Construct String from Binary Tree.go

---
+++ leetcode/Construct String from Binary Tree.go
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func tree2str(t *TreeNode) string {
+    if t == nil {return ""}
+    var res = fmt.Sprintf("%v", t.Val)
+    if t.Left != nil && t.Right != nil {
+        res += fmt.Sprintf("(%v)(%v)", tree2str(t.Left), tree2str(t.Right))
+    } else if t.Left != nil {
+        res += fmt.Sprintf("(%v)", tree2str(t.Left))
+    } else if t.Right != nil {
+        res += fmt.Sprintf("()(%v)", tree2str(t.Right))
+    }
+    return res
+}
