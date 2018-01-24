819365cf82c31225da5b2a5d6dc80d7ac59f12ea
Dapangmao <hchao8@gmail.com>
2018-01-24 18:03:07 -0500

    Create Rotate Image.go

---
+++ leetcode/Rotate Image.go
@@ -0,0 +1,9 @@
+func rotate(matrix [][]int)  {
+    var n = len(matrix)
+    for i:=0; i<n/2; i++ {
+        for j:=i; j<n-i-1; j++ {
+            matrix[i][j], matrix[j][n-i-1], matrix[n-i-1][n-j-1], matrix[n-j-1][i] = 
+                matrix[n-j-1][i], matrix[i][j], matrix[j][n-i-1], matrix[n-i-1][n-j-1]
+        }
+    }
+}
