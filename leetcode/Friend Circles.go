e89d4d280d57b5d979dfb9f191ecd4f0f7806e6f
Dapangmao <hchao8@gmail.com>
2018-02-16 12:21:52 -0500

    Update Friend Circles.go

--- leetcode/Friend Circles.go
+++ leetcode/Friend Circles.go
@@ -1,17 +1,27 @@
-var visited = make(map[int]bool)
-
 func findCircleNum(M [][]int) int {
     n, m := len(M), len(M[0])
+    var visited = make(map[int]bool)
+
     graph := make(map[int][]int)
     for i:=0; i<n; i++ {
         for j:=0; j<m; j++ {
-            if j > i {continue}   
+            if j > i {continue}  // don't give up those orpahns 
             if M[i][j] == 1 {
                 graph[i] = append(graph[i], j)  
                 graph[j] = append(graph[j], i) 
             }
         }
     }
+    
+    var dfs func(i int, graph map[int][]int)
+    dfs = func(i int, graph map[int][]int) {
+        if _, ok := visited[i]; ok {return}
+        visited[i] = true
+        for _, j := range graph[i] {
+            dfs(j, graph)
+        }
+    }
+    
     var res int
     for k, _ := range graph {
         if _, ok := visited[k]; ok {continue}
@@ -19,12 +29,4 @@ func findCircleNum(M [][]int) int {
         res++
     }
     return res
-}
-
-func dfs(i int, graph map[int][]int) {
-    if _, ok := visited[i]; ok {return}
-    visited[i] = true
-    for _, j := range graph[i] {
-        dfs(j, graph)
-    }
 }
