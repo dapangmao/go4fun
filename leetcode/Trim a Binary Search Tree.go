ec6d9a6041a27866b64369d572e763cd73a459d9
Dapangmao <hchao8@gmail.com>
2018-02-23 08:33:53 -0500

    Create Trim a Binary Search Tree.go

---
+++ leetcode/Trim a Binary Search Tree.go
@@ -0,0 +1,8 @@
+func trimBST(root *TreeNode, L int, R int) *TreeNode {
+    if root == nil {return nil}
+    if root.Val > R {return trimBST(root.Left, L, R)}
+    if root.Val < L {return trimBST(root.Right, L, R)}
+    root.Left = trimBST(root.Left, L, R)
+    root.Right = trimBST(root.Right, L, R)
+    return root
+}
