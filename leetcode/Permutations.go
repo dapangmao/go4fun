fc95e9425aa4c7284089812aaeeca91503303a19
Dapangmao <hchao8@gmail.com>
2018-02-25 09:29:01 -0500

    Update Permutations.go

--- leetcode/Permutations.go
+++ leetcode/Permutations.go
@@ -1,14 +1,9 @@
 func permute(nums []int) [][]int {
     n := len(nums)
     if n == 1 {return [][]int{nums}}
-    if n == 2 {
-        return [][]int{nums, []int{nums[1], nums[0]}}
-    }
-
     var res [][]int    
-    for i, x := range nums {
-        var others []int
-        others = append(append(others, nums[:i]...), nums[i+1:]...)
+    for i, x := range nums {  
+        var others = append(append([]int{}, nums[:i]...), nums[i+1:]...)
         for _, y := range permute(others) {
             res = append(res, append(y, x))
         }
