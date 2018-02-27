8c9f21dd2dbb6129ad68888da4c15e8ea60b85b9
Dapangmao <hchao8@gmail.com>
2018-02-27 15:09:32 -0500

    Update Reverse Linked List.go

--- Reverse Linked List.go
+++ Reverse Linked List.go
@@ -7,7 +7,12 @@
  */
 func reverseList(head *ListNode) *ListNode {
     if head == nil || head.Next == nil {return head}
-    prev := new(ListNode).Next
+    // (1) untyped nil does not work
+    // prev := nil 
+    // (2) This works
+    // prev := new(ListNode).Next
+    // (3) Best approach
+    var prev *ListNode
     for head != nil {
         current := head.Next
         head.Next = prev
