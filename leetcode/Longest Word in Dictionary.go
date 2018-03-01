8d5566c932ad4339e5d58bb0ca7167787b6eb362
Dapangmao <hchao8@gmail.com>
2018-02-28 20:58:43 -0500

    Update Longest Word in Dictionary.go

--- leetcode/Longest Word in Dictionary.go
+++ leetcode/Longest Word in Dictionary.go
@@ -1,20 +1,15 @@
 func longestWord(words []string) string {
-    set := make(map[string]int)
-    for _, x := range words {
-        set[x] = len(x)
-    }
+    set := make(map[string]bool)
+    for _, x := range words {set[x] = true}
     ans := ""
     for _, word := range words {
-        if set[word] < set[ans] || (set[word] == set[ans] && word >= ans) {
-            continue
-        willAdd := true
-        for k:=1; k<set[word]; k++ {
-            if _, ok := set[word[:k]]; !ok {
-                willAdd = false
-                break
-            } 
+        if len(word) > len(ans) || len(word) == len(ans) && word < ans {
+            for k:=1; k<len(word); k++ {
+                if !set[word[:k]] {goto end} 
+            }
+            ans = word
+            end:
         }
-        if willAdd {ans = word}
     }
     return ans
 }
