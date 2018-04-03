3e962a6a3082ab96a1f7aa32608560b72558bcdb
Dapangmao <hchao8@gmail.com>
2018-04-03 09:56:03 -0400

    Update Reverse Linked List.go

--- leetcode/Reverse Linked List.go
+++ leetcode/Reverse Linked List.go
@@ -9,8 +9,13 @@
 
 // more information can be found at TGPL 7.5.1
 // **** an interface containing a Nil pointer is not nil ***
-// such as new(ListNode)
-// otherwise using *ListNode
+// func main() {
+// 	var a *ListNode
+// 	fmt.Println(a)
+// 	var b = new(ListNode)
+// 	fmt.Println(b)
+// }
+
 func reverseList(head *ListNode) *ListNode {
     if head == nil || head.Next == nil {return head}
     var prev *ListNode
