cfda7de3cc3cd83f8cdbf68fb7960685b91f81e7
Dapangmao <hchao8@gmail.com>
2018-04-09 14:36:17 -0400

    Update Convert BST to Greater Tree.go

--- leetcode/Convert BST to Greater Tree.go
+++ leetcode/Convert BST to Greater Tree.go
@@ -1,4 +1,19 @@
 func convertBST(root *TreeNode) *TreeNode {
+    var s = 0
+    dfs(root, &s)
+    return root
+}
+
+func dfs (root *TreeNode, sum *int) {
+    if root == nil {return}
+    dfs(root.Right, sum)
+    *sum += root.Val
+    root.Val = *sum
+    dfs(root.Left, sum)
+}
+
+// functional but 3 times slower
+func convertBST(root *TreeNode) *TreeNode {
     var sum = 0
     var dfs func(root *TreeNode) 
     dfs = func(root *TreeNode) {
