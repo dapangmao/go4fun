6ef36acd9bc81062c4098b488131b924663a5506
Dapangmao <hchao8@gmail.com>
2018-08-11 17:35:21 -0400

    Update My Calendar I.go

--- leetcode/My Calendar I.go
+++ leetcode/My Calendar I.go
@@ -9,12 +9,21 @@ func Constructor() MyCalendar {
     return MyCalendar{[]int{}, []int{}, 0, map[int]int{}}
 }
 
+
+func (this *MyCalendar) add(arr *[]int, i, num int) []int {
+    var res = make([]int, this.n+1)
+    copy(res, (*arr)[:i])
+    copy(res[i+1:], (*arr)[i:])
+    res[i] = num
+    return res
+}
+
 func (this *MyCalendar) Book(start int, end int) bool {
     if _, ok := this.sdic[start]; ok {return false}
     var i = sort.SearchInts(this.starts, start)
     if  (i < this.n && end > this.starts[i]) || (i > 0  && start < this.ends[i-1]) {return false}
-    this.starts = append(this.starts[:i], append([]int{start}, this.starts[i:]...)...)
-    this.ends = append(this.ends[:i], append([]int{end}, this.ends[i:]...)...)
+    this.starts = this.add(&this.starts, i, start)
+    this.ends = this.add(&this.ends, i, end)
     this.n++
     this.sdic[start] = 0
     return true
