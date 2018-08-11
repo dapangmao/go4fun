0aa0cbfdf93ba8230fedaa55e85b2759d4846a49
Dapangmao <hchao8@gmail.com>
2018-08-11 17:51:14 -0400

    Update Sum of Left Leaves.go

--- leetcode/Sum of Left Leaves.go
+++ leetcode/Sum of Left Leaves.go
@@ -7,16 +7,18 @@
  * }
  */
 
-var res int
 
 func sumOfLeftLeaves(root *TreeNode) int {
+    var res int
+    var dfs func(root *TreeNode, isLeft bool)
+    dfs = func(root *TreeNode, isLeft bool) {
+        if root == nil {return}
+        if isLeft && root.Left == nil && root.Right == nil {
+            res += root.Val
+        }
+        dfs(root.Left, true)
+        dfs(root.Right, false)
+    }
     dfs(root, false)
     return res
-}
-
-func dfs(root *TreeNode, isLeft bool) {
-    if root == nil {return}
-    if isLeft {res += root.Val}
-    dfs(root.Left, true)
-    dfs(root.Right, false)
 }
