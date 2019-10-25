2be40efe4208ae2e14213c8cea7992577c7c4d87
Dapangmao <hchao8@gmail.com>
2019-10-25 16:29:39 -0400

    Update Daily Temperatures.go

--- leetcode/Daily Temperatures.go
+++ leetcode/Daily Temperatures.go
@@ -1,17 +1,15 @@
 func dailyTemperatures(temperatures []int) []int {
 	var n = len(temperatures)
-	var res, stack []int
-	for i:=0; i<n; i++ {
-		res = append(res, 0)
+	var stack []int
+	res := make([]int, n)
+	for i:=n-1; i>=0; i-- {
+	for len(stack) > 0 && temperatures[i] >= temperatures[stack[len(stack)-1]] {
+	    stack = stack[:len(stack)-1]
 	}
-    for i:=n-1; i>=0; i-- {
-        for len(stack) > 0 && temperatures[i] >= temperatures[stack[len(stack)-1]] {
-            stack = stack[:len(stack)-1]
-        }
-        if len(stack) > 0 {
-            res[i] = stack[len(stack)-1] - i
-        }
-        stack = append(stack, i)
-    }
-    return res
+	if len(stack) > 0 {
+	    res[i] = stack[len(stack)-1] - i
+	}
+	stack = append(stack, i)
+	}
+	return res
 }
