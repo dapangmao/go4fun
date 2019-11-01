a586f4581277b7e3ddec653453619323014e8c55
Dapangmao <hchao8@gmail.com>
2019-11-01 15:45:24 -0400

    Update Baseball Game.go

--- leetcode/Baseball Game.go
+++ leetcode/Baseball Game.go
@@ -1,16 +1,16 @@
 func calPoints(ops []string) int {
     var stack []int
     for _, op := range ops {
-        var n = len(stack)
+        n := len(stack)
         if op == "+" {
-            stack = append(stack, stack[n-1], stack[n-2])
+            stack = append(stack, stack[n-1] + stack[n-2])
         } else if op == "C" {
             stack = stack[:n-1]
         } else if op == "D" {
             stack = append(stack, 2*stack[n-1])
         } else {
-            opint, _ := strconv.Atoi(op)
-            stack = append(stack, opint)
+            e, _ := strconv.Atoi(op)
+            stack = append(stack, e)
         }
     }
     var res = 0
